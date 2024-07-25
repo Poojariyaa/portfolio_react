ed98536b05e0e1abed1761d3f2aae1d3eebfb585
Poojariyaa <poojavmay2004@gmail.com>
2024-07-25 14:51:31 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <section id="home" className="text-center container">
+            <h1>Welcome to My Portfolio</h1>
+            <p>Hi, I'm Pooja V. A passionate developer from Arakkonam.</p>
+        </section>
+    );
+}
+
+export default Home;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,23 @@
+// src/components/Projects.js
+import React from 'react';
+import './Projects.css';
+
+function Projects() {
+    return (
+        <section id="projects">
+            <h2>My Projects</h2>
+            <h1>Educational Chatbot Application (using java)</h1>
+            <ul>
+                <li>
+                An educational chatbot is a digital assistant designed to aid learning by answering questions, 
+                providing explanations, and offering educational resources. It can be programmed to handle 
+                various subjects, offer interactive quizzes, and guide users through complex topics.
+                The chatbot uses natural language processing (NLP) to understand and respond to user inquiries
+                effectively.</li>
+                
+            </ul>
+        </section>
+    );
+}
+
+export default Projects;
