53259ef440a5c9d02177c1ea307aab7b1669826f
Poojariyaa <poojavmay2004@gmail.com>
2024-07-25 19:50:04 +0530

    first commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,15 +6,15 @@ function Projects() {
     return (
         <section id="projects">
             <h2>My Projects</h2>
-            <h1>Educational Chatbot Application (using java)</h1>
+            <h1>Educational Chatbot Application</h1>
             <ul>
                 <li>
                 An educational chatbot is a digital assistant designed to aid learning by answering questions, 
                 providing explanations, and offering educational resources. It can be programmed to handle 
                 various subjects, offer interactive quizzes, and guide users through complex topics.
                 The chatbot uses natural language processing (NLP) to understand and respond to user inquiries
-                effectively.</li>
-                
+                effectively.
+                </li>
             </ul>
         </section>
     );
